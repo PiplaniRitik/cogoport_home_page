7d5b96604cf81e7ad4a35fe3b063abcaa25eba4b
PiplaniRitik <96330982+PiplaniRitik@users.noreply.github.com>
2023-07-06 23:52:45 +0530

    styling going on

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Notification.css';
 const Alert = () => {
   const [showAlert, setShowAlert] = useState(true);
-  const [containerBackground, setContainerBackground] = useState('red');
+  const [containerBackground, setContainerBackground] = useState('#ee3425');
   const handleClose = () => {
     setShowAlert(false);
     setContainerBackground('yellow');
@@ -10,11 +10,12 @@ const Alert = () => {
 
   return showAlert ? (
     <div className="alert" style={{ backgroundColor: containerBackground }}>
-      <div className="logo">
-        <img src="path/to/logo.png" alt="Logo" />
-      </div>
+      
       <div className="alert-text">
-        <p>Limited Time Offer: Get 15% off on all Cogo Assured Rates. <a href="#" className="book-link">Book today</a></p>
+      <div className="logo">
+      <img alt="icon" loading="lazy" width="16" height="16" decoding="async" data-nimg="1" className="styles_icon__ktx3Z" srcset="/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&amp;w=16&amp;q=75 1x, /_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&amp;w=32&amp;q=75 2x" src="/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&amp;w=32&amp;q=75" style={{color: "transparent"}}/>
+      </div>
+        <p><strong>Limited Time Offer: Get 15% off on all Cogo Assured Rates. <a href="#" className="book-link">Book today</a></strong></p>
       </div>
       <div className="close-btn" onClick={handleClose}>
       &#x2715;
